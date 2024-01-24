a2672169cbfb4e02711b38a3c9d56a46ea029311
Turki <85177058+Turki25@users.noreply.github.com>
2024-01-24 03:29:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function getRandomQuote() {
-    fetch('https://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('quote').innerText = data.content;
-        })
-        .catch(error => console.error('Error fetching quote:', error));
-}
